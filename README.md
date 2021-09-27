4b8a00b59e775a008c638f0fb58e0f719f4a0ae4
Bob Caddy <r.Caddy@pitt.edu>
2021-09-27 15:14:59 -0400

    Remove all old settings

--- README.md
+++ README.md
@@ -1,18 +1,10 @@
 # WARNING
-This repo has been archived as it is no longer compatible with Cholla and has been 
+This repo has been archived as it is no longer compatible with Cholla and has been
 superseded by Cholla's new build system.
 
-# Cholla-Makefiles
-This is a repo to store my Makefiles for cholla.
-
+# Editor Settings for Cholla
+Contains settings for various text editors and IDE's (mostly VSC) to use with
+Cholla
 
 ## Dependencies:
-* GNU Make
-
-## Summary
-Makefiles are sensically named an can be linked from the local directory for cholla.
-
-## Setup Instructions
-Create a link `~/cholla/Makefile` that points to whichever Makefile you want to
-use in this repo.
-`ln -s <repo-root>/<Makefile of choice> <cholla-root>/Makefile`
+* Bash
--- link-files.sh
+++ link-files.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 
 # Description:
-#   This script automatically links all the files and directories that Cholla
-#   needs.  This includes the Makefile and IDE directories only at the moment.
-#   Those files all are tracked and reside in this repo but need to be 
+#   This script automatically links all the files and directories that I need for my settings
+#   needs.  This includes the IDE directories only at the moment.
+#   Those files all are tracked and reside in this repo but need to be
 #   accessible in the Cholla directory.
 #
 # Dependencies:
-#   Requires that the Cholla repo is right next to the cholla-Makefile repo
+#   Requires that the Cholla repo is right next to this repo
 #
-# Args:
-#   $1 - The system being used so that the correct Makefile can be linked
 
-#set -x #echo all commands
+set -x #echo all commands
 
 # Find the paths
 REPO_ROOT=$(git rev-parse --show-toplevel)
-CHOLLA_ROOT="${REPO_ROOT::-9}"
+CHOLLA_ROOT="${REPO_ROOT::-22}"
 
 LN="$(which ln)"
 LN_Flags=(
@@ -24,26 +22,13 @@ LN_Flags=(
     --force
 )
 
-if [ "${1,,}" == "h2p" ]; then
-    Makefile_Target="h2p-Makefile"
-# elif [ "${1,,}" == "ANOTHER SYSTEM" ]; then
-else
-    printf "Error: No system specified or system does not exist."
-    exit 1
-fi
-
-
 # Set up arrays for the objects to link
 Files_to_link=(
-    "${Makefile_Target}"
     vscode
-    idea
 )
 
 Link_Names=(
-    Makefile
     .vscode
-    .idea
 )
 
 # Check if the arrays are the same size
